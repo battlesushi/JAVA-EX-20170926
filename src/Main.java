3da6640f83e8e3879f5866ac4dbb802b9e39a0e7
battlesushi <105021030@live.asia.edu.tw>
2017-09-26 12:22:43 +0800

    926

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        MainFrame mFrm=new MainFrame();
+        mFrm.setVisible(true);
     }
 }
---
+++ src/MainFrame.java
@@ -0,0 +1,89 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MainFrame extends Frame {
+    private Button btnAdd = new Button("Add");
+    private Button btnExit = new Button("Exit");
+    private Button btnSub = new Button("Sub");
+    private Button btnStop = new Button("Stop");
+    private Label lab = new Label("@_@");
+    private Timer t1;
+    private boolean flag=true;
+    private int labx = 50, laby = 50;
+    public MainFrame() {
+        init();
+    }
+
+    public void init() {
+        this.setLayout(null);
+        setBounds(100, 100, 500, 400);
+        this.add(lab);
+        lab.setBounds(50, 50, 100, 30);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.add(btnAdd);
+        btnAdd.setBounds(100, 200, 50, 30);
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                t1.start();
+            }
+        });
+
+        this.add(btnSub);
+        btnSub.setBounds(175, 200, 50, 30);
+        btnSub.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                t1.start();
+                flag=false;
+            }
+        });
+
+        this.add(btnExit);
+        btnExit.setBounds(250, 200, 50, 30);
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        this.add(btnStop);
+        btnStop.setBounds(325, 200, 50, 30);
+        btnStop.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                t1.stop();
+            }
+        });
+        t1 = new Timer(50, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+                if(flag){
+                    labx+=5;
+                    lab.setLocation(labx, laby);
+                    if(labx>MainFrame.this.getWidth()){
+                        flag=false;
+                    }
+                }
+                else if(!flag){
+                    labx-=5;
+                    lab.setLocation(labx, laby);
+                    if(labx<0){
+                        flag=true;
+                    }
+                }
+
+            }
+        });
+    }
+}
